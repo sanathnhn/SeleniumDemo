cfabe5f1cc2e47a1187ffb73e50e2901cfe4ae7e
unknown <sanathhn4116@gmail.com>
2023-12-26 22:53:27 +0530

    sanath

---
+++ src/test/java/App.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.javatpoint.com/");
+	final String  name="sanath";
+}
+}
